551ce2beeede2c73a04a634ea02bd5d4a4a71097
Blizzorde <119112639+Blizzorde@users.noreply.github.com>
2026-05-18 03:23:30 -0300

    Added JS for Automatic Active Classes for Navbar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach((link) => {
+  link.classList.remove("active");
+
+  if (link.href.includes(window.location.pathname.split("/").pop())) {
+    link.classList.add("active");
+  }
+});
+
 function showToast(message = "Project link not available yet") {
   const toast = document.getElementById("toast");
   toast.textContent = message;
